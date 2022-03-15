28517f42b7c2249a2e1aa353c0336f86d8c11fbf
Akash jaga <akashjagadish22@gmail.com>
2022-03-15 14:53:47 +0530

    okie

---
+++ script.js
@@ -0,0 +1,21 @@
+var psw = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+function money () {
+    if (<= 60000){
+        alert("money entered is invalid")
+    }else{
+        alert("entered correct money")
+    }
+}
